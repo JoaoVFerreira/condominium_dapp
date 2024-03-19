cfafaba4c5968b38a1eeaec61a812c8fc2b93da7
João Ferreira <joaovtf39@gmail.com>
2024-03-19 20:12:05 -0300

    feat: add manager verification

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,6 +1,8 @@
 import { Route, BrowserRouter, Routes, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Topics from './pages/Topics';
+import Transfer from './pages/Transfer';
+import { Profile, doLogout } from './services/Web3Service';
 
 function Router() {
   type Props = {
@@ -8,8 +10,20 @@ function Router() {
   }
 
   function PrivateRoute({ children }: Props) {
-    const isAuth = localStorage.getItem("account") !== null;
+    const isAuth = !!localStorage.getItem("account");
     return isAuth ? children : <Navigate to="/"/>;
+  }
+
+  function ManagerRoute({ children }: Props) {
+    const isAuth = !!localStorage.getItem("account");
+    const isManager = Number(localStorage.getItem('profile') ?? Profile.RESIDENT) === Profile.MANAGER;
+
+    if (isAuth && isManager) {
+      return children;
+    } else {
+      doLogout();
+      return <Navigate to="/"/>;
+    }
   }
 
   return (
@@ -20,6 +34,11 @@ function Router() {
           <PrivateRoute>
               <Topics/>
           </PrivateRoute>
+        } />
+        <Route path='/transfer' element={
+          <ManagerRoute>
+              <Transfer/>
+          </ManagerRoute>
         } />
       </Routes>
     </BrowserRouter>
--- frontend/src/services/Web3Service.ts
+++ frontend/src/services/Web3Service.ts
@@ -1,15 +1,53 @@
 import { ethers } from 'ethers';
+import ABI from './ABI.json';
+
+const ADAPTER_ADDRESS = `${process.env.REACT_APP_ADAPTER_ADDRESS}`
+
+export enum Profile {
+  RESIDENT = 0,
+  COUNSELOR = 1,
+  MANAGER = 2
+}
+
+export type LoginResult = {
+  account: string;
+  profile: Profile
+}
 
 function getProvider(): ethers.BrowserProvider {
   if (!window.ethereum) throw new Error('No MetaMask found!');
   return new ethers.BrowserProvider(window.ethereum);
 }
 
-export async function doLogin(): Promise<string> {
+function getContract(provider?: ethers.BrowserProvider): ethers.Contract {
+  if (!provider) provider = getProvider();
+  return new ethers.Contract(ADAPTER_ADDRESS, ABI, provider);
+}
+
+export async function doLogin(): Promise<LoginResult> {
   const provider = getProvider();
   const accounts = await provider.send('eth_requestAccounts', []);
   if (!accounts?.length) throw new Error('Wallet not allowed!');
-  localStorage.setItem('account', accounts[0]);
+
+  const contract = getContract(provider);
+  const manager = (await contract.getManager()) as string;
+  const isManager = manager.toUpperCase() === accounts[0].toUpperCase();
+
+  if (isManager) {
+    localStorage.setItem('profile', String(Profile.MANAGER));  
+  } else {
+    localStorage.setItem('profile', String(Profile.RESIDENT))
+  }
+
+  localStorage.setItem('account', accounts[0].toUpperCase());
   
-  return accounts[0];
+  return {
+    account: accounts[0],
+    profile: Number(localStorage.getItem('profile') ?? Profile.RESIDENT)
+  }
+}
+
+export function doLogout() {
+  localStorage.removeItem('account');
+  localStorage.removeItem('profile');
 }
